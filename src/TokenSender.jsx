82b3161f86ea9f76c189b958c92a226a656bb209
vishal5208 <vishald5208@gmail.com>
2024-03-22 15:52:16 +0530

    showed balance

--- src/TokenSender.jsx
+++ src/TokenSender.jsx
@@ -10,6 +10,7 @@ function TokenSender({ wallet }) {
     amount: localStorage.getItem('amount') || '',
     contractAddress: localStorage.getItem('contractAddress') || '',
     balance: localStorage.getItem('balance') || 0,
+    tokenSymbol: '', // Initialize token symbol state
   };
 
   const [state, setState] = useState(initialState);
@@ -20,18 +21,32 @@ function TokenSender({ wallet }) {
   };
 
   const handleCheckBalance = async () => {
-    const { wallet, contractAddress } = state;
+    const {contractAddress } = state;
+
     if (!wallet || !contractAddress) return;
 
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner(wallet);
+    console.log(signer)
     const contract = new ethers.Contract(contractAddress, erc20Abi, signer);
     
-    const userBalance = await contract.balanceOf(wallet);
-    const formattedBalance = ethers.utils.formatEther(userBalance);
-    setState({ ...state, balance: formattedBalance });
-    // Save balance to localStorage
-    localStorage.setItem('balance', formattedBalance);
+    try {
+      // Fetch token symbol
+      const tokenSymbol = await contract.symbol();
+    
+      console.log(tokenSymbol)
+
+      // Fetch user balance
+      const userBalance = await contract.balanceOf(wallet);
+      const formattedBalance = ethers.utils.formatUnits(userBalance, await contract.decimals());
+
+      setState({ ...state, balance: formattedBalance, tokenSymbol });
+      // Save balance to localStorage
+      localStorage.setItem('balance', formattedBalance);
+    } catch (error) {
+      console.error('Error checking balance:', error);
+      alert('Failed to check balance.');
+    }
   };
 
   const handleSend = async () => {
@@ -43,7 +58,7 @@ function TokenSender({ wallet }) {
     const contract = new ethers.Contract(contractAddress, erc20Abi, signer);
     
     try {
-      const tx = await contract.transfer(recipient, ethers.utils.parseEther(amount));
+      const tx = await contract.transfer(recipient, ethers.utils.parseUnits(amount, await contract.decimals()));
       await tx.wait();
       alert('Tokens sent successfully!');
     } catch (error) {
@@ -89,8 +104,8 @@ function TokenSender({ wallet }) {
       <Button variant="contained" color="primary" onClick={handleCheckBalance} style={{ marginBottom: '10px' }}>
         Check Balance
       </Button>
-      <p>Your balance: {state.balance} ETH</p>
-      <p>Sender: {wallet}</p> {/* Display the connected wallet address as the sender */}
+      <p>Your balance: {state.balance} {state.tokenSymbol}</p> {/* Display token symbol */}
+      <p>Sender: {wallet}</p> 
       <Button variant="contained" color="primary" onClick={handleSend}>
         Send Tokens
       </Button>
--- src/erc20Abi.js
+++ src/erc20Abi.js
@@ -1,44 +1,66 @@
-
 const erc20Abi = [
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "_to",
-          "type": "address"
-        },
-        {
-          "name": "_value",
-          "type": "uint256"
-        }
-      ],
-      "name": "transfer",
-      "outputs": [
-        {
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [
-        {
-          "name": "_owner",
-          "type": "address"
-        }
-      ],
-      "name": "balanceOf",
-      "outputs": [
-        {
-          "name": "balance",
-          "type": "uint256"
-        }
-      ],
-      "type": "function"
-    }
-  ];
-  
-  export default erc20Abi;
-  
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "_owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "name": "balance",
+        "type": "uint256"
+      }
+    ],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "decimals", // Added decimals function
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "type": "function"
+  }
+];
+
+export default erc20Abi;
